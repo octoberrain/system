octoberrain/system

commit 465a7b8891ea83740c5ced37cd99406a73a3b66e

Make media URLs in accordance with the Link Policy (#3536)
Credit to @adsa95

classes/MediaLibrary.php
@@ -6,6 +6,7 @@ use Cache;
 use Config;
 use Storage;
 use Request;
+use Url;
 use October\Rain\Filesystem\Definitions as FileDefinitions;
 use ApplicationException;
 use SystemException;
@@ -538,7 +539,9 @@ class MediaLibrary
     {
         $path = $this->validatePath($path);
 
-        return $this->storagePath.$path;
+        $fullPath = $this->storagePath.implode("/", array_map("rawurlencode", explode("/", $path)));
+
+        return Url::to($fullPath);
     }
 
     /**

commit 3de9c5920f6fd8437c5a5b01498aaa72c07b537d

Only force absolute URLs for media items if cms.linkPolicy = force
This fixes #4082 & rainlab/pages-plugin#405. Reverts a previous breaking change from Build 444 (introduced in #3536) by limiting the scope to which it applies.

classes/MediaLibrary.php
@@ -537,9 +537,13 @@ class MediaLibrary
     {
         $path = $this->validatePath($path);
 
-        $fullPath = $this->storagePath.implode("/", array_map("rawurlencode", explode("/", $path)));
+        $fullPath = $this->storagePath . implode("/", array_map("rawurlencode", explode("/", $path)));
 
-        return Url::to($fullPath);
+        if (Config::get('cms.linkPolicy') === 'force') {
+            return Url::to($fullPath);
+        } else {
+            return $fullPath;
+        }
     }
 
     /**